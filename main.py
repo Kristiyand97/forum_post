c3ad44d2076f25722099677f1aa55af2d5d6a65c
penevsimeon <penevsimeon99@gmail.com>
2024-04-28 17:29:39 +0300

    set the connection_db

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers.topics import topics_router
 from routers.users import users_router
+import uvicorn
 
 app = FastAPI()
 app.include_router(users_router)
@@ -14,5 +15,5 @@ app.include_router(topics_router)
 
 
 
-# if __name__ == '__main__':
-#     uvicorn.run('main:app', host='127.0.0.1', port=8022, reload=True)
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8022, reload=True)

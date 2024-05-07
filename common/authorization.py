aa97d15e97bef517e04efbc5b73e3938dac449b9
IvoBelchev96 <ivo.belchev.a57@learn.telerikacademy.com>
2024-05-07 17:40:42 +0300

    Changed the token so we can use swagger UI

--- common/authorization.py
+++ common/authorization.py
@@ -1,5 +1,5 @@
 from fastapi import Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from data.schemas import TokenData
@@ -8,7 +8,7 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+bearer_scheme = HTTPBearer()
 
 
 def create_access_token(data: dict):
@@ -19,9 +19,9 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 
-def verify_access_token(token: str, credentials_exception):
+def verify_access_token(token: HTTPAuthorizationCredentials, credentials_exception):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token.credentials, SECRET_KEY, algorithms=ALGORITHM)
         user_id: str = payload.get("user_id")
 
         if user_id is None:
@@ -33,7 +33,7 @@ def verify_access_token(token: str, credentials_exception):
 
 
 # verifies if the token is correct
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: HTTPAuthorizationCredentials = Depends(bearer_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
--- data/connection.py
+++ data/connection.py
@@ -5,7 +5,7 @@ def _get_connection():
     try:
         conn = connect(
             user='root',
-            password='1234',
+            password='2210',
             host='localhost',
             port=3306,
             database='forum_system_schema'
